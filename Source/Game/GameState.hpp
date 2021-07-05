13bce1d687812fcf0a521f4aedaf77f30af4158d
Kishimi <anton@green-pr.org>
2021-07-05 13:27:24 +0200

    Removed `window` parameter from Render method

--- Source/Game/GameState.hpp
+++ Source/Game/GameState.hpp
@@ -11,7 +11,7 @@ public:
 
 public:
 	virtual GameState* Update(const float deltaTime) = 0;
-	virtual void Render(sf::RenderWindow &window) = 0;
+	virtual void Render() = 0;
 
 	virtual void OnEvent(const sf::Event &event) {}
 };
