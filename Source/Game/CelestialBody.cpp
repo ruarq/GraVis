649110233681bc0a182ea683bbc374f1d56a5baf
Kishimi <anton@green-pr.org>
2021-07-09 23:37:49 +0200

    Removed gradient lambda

--- Source/Game/CelestialBody.cpp
+++ Source/Game/CelestialBody.cpp
@@ -22,11 +22,8 @@ void CelestialBody::Render(sf::RenderWindow &window)
 		std::uint32_t i = 0;
 		for (const sf::Vector2f &position : path)
 		{
-			// gradient function
-			auto f = [](const float x) { return -std::pow(x - 1.0f, 8.0f) + 1.0f; };
-
 			const float x = i++ / float(path.size());
-			const float gradient = f(x);
+			const float gradient = -std::pow(x - 1.0f, 8.0f) + 1.0f;
 			
 			sf::Vertex vertex = position;
 			vertex.color = sf::Color(gradient * pathColor.r, gradient * pathColor.g, gradient * pathColor.b);
